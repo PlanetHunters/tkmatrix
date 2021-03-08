32ad22e7ee246920a13aa6cdd049f05fbf645cbd
martin.dvpj <martin.devora.pajares@gmail.com>
2021-03-08 11:19:29 +0000

    Preparing release 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-version = "0.1"
+version = "0.1.0"
 
 setuptools.setup(
     name="tirma", # Replace with your own username
