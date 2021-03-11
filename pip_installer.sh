ba87b3144ab1258295081dd719869fd3fc4f8cf1
martin.dvpj <martin.devora.pajares@gmail.com>
2021-03-11 17:37:46 +0000

    updated tkmatrix

--- pip_installer.sh
+++ pip_installer.sh
@@ -3,9 +3,9 @@
 rm tests.log
 rm dist* -r
 set -e
-#tox -r > tests.log
-#tests_results=$(cat tests.log | grep "congratulations")
-#if ! [[ -z ${tests_results} ]]; then
+tox -r > tests.log
+tests_results=$(cat tests.log | grep "congratulations")
+if ! [[ -z ${tests_results} ]]; then
   git_tag=$1
   sed -i '5s/.*/version = "'${git_tag}'"/' setup.py
   git add setup.py
@@ -14,7 +14,7 @@ set -e
   git push
   python3 setup.py sdist bdist_wheel
   python3 -m twine upload dist/*
-#else
-#  echo "TESTS FAILED. See tests.log"
-#fi
+else
+  echo "TESTS FAILED. See tests.log"
+fi
 rm dist* -r
