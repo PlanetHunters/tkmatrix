d09d34eeb9c6e1f8766353469e8d9028bc255bd1
martin.dvpj <martin.devora.pajares@gmail.com>
2021-05-28 20:48:55 +0100

    update tkmatrix_class.py plots

--- tkmatrix/tkmatrix_class.py
+++ tkmatrix/tkmatrix_class.py
@@ -63,12 +63,12 @@ class MATRIX:
         print('\n STELLAR PROPERTIES FOR THE SIGNAL SEARCH')
         print('================================================\n')
         print('limb-darkening estimates using quadratic LD (a,b)=', self.ab)
-        print('mass =', format(self.mstar,'0.5f'))
-        print('mass_min =', format(self.mstar_min,'0.5f'))
-        print('mass_max =', format(self.mstar_max,'0.5f'))
-        print('radius =', format(self.rstar,'0.5f'))
-        print('radius_min =', format(self.rstar_min,'0.5f'))
-        print('radius_max =', format(self.rstar_max,'0.5f'))
+        print('mass =', format(self.mstar, '0.5f'))
+        print('mass_min =', format(self.mstar_min, '0.5f'))
+        print('mass_max =', format(self.mstar_max, '0.5f'))
+        print('radius =', format(self.rstar, '0.5f'))
+        print('radius_min =', format(self.rstar_min, '0.5f'))
+        print('radius_max =', format(self.rstar_max, '0.5f'))
 
     def build_inject_dir(self):
         inject_dir = self.dir + "/" + self.object_info.mission_id().replace(" ", "") + "_ir/"
@@ -337,8 +337,8 @@ class MATRIX:
             shape_1='sphere', shape_2='sphere',
             spots_1=None, spots_2=None,
             exact_grav=False, verbose=1)
-        flux_t = flux + model - 1.
         if model[0] > 0:
+            flux_t = flux + model - 1.
             result_flux = flux_t
             result_flux_err = flux_err
             result_time = time
@@ -395,24 +395,26 @@ class MATRIX:
         max_rad = df["radius"].max()
         phases = len(df[df["period"] == df["period"].min()][df["radius"] == df["radius"].min()])
         phases_str = "phase" if phases == 1 else "phases"
+        bin_nums = int(np.ceil(len(df["period"].unique()) / binning))
         if period_grid_geom == 'lin':
             step_period = (max_period - min_period) / (len(df["period"].unique()) - 1)
             step_period = step_period * binning
             if step_period <= 0:
                 step_period = 0.1
-            period_grid = np.arange(min_period, max_period + step_period / binning + 0.01,
-                                    step_period) if max_period - min_period > 0 else np.full((1), min_period)
+            period_grid = np.linspace(min_period, max_period, bin_nums)\
+                if max_period - min_period > 0 else np.full((1), min_period)
         else:
-            period_grid = np.logspace(np.log10(min_period), np.log10(max_period), len(df["period"].unique()))
+            period_grid = np.logspace(np.log10(min_period), np.log10(max_period), bin_nums)
+        bin_nums = int(np.ceil(len(df["radius"].unique()) / binning))
         if radius_grid_geom == 'lin':
             step_radius = (max_rad - min_rad) / (len(df["radius"].unique()) - 1)
             step_radius = step_radius * binning
             if step_radius <= 0:
                 step_radius = 0.1
-            radius_grid = np.arange(min_rad, max_rad + 0.01 + step_radius / binning,
-                                    step_radius) if max_rad - min_rad > 0 else np.full((1), min_rad)
+            radius_grid = np.round(np.linspace(min_rad, max_rad, bin_nums), 2)\
+                if max_rad - min_rad > 0 else np.full((1), min_rad)
         else:
-            radius_grid = np.logspace(np.log10(min_rad), np.log10(max_rad), len(df["radius"].unique()))
+            radius_grid = np.round(np.logspace(np.log10(min_rad), np.log10(max_rad), 2), bin_nums)
         f = len(period_grid) / len(radius_grid)
         bins = [period_grid, radius_grid]
         h1, x, y = np.histogram2d(df['period'][df['found'] == 1], df['radius'][df['found'] == 1], bins=bins)
@@ -436,6 +438,31 @@ class MATRIX:
             plt.xticks(yticks)
         plt.savefig(inject_dir + '/inj-rec.png', bbox_inches='tight', dpi=200)
         plt.close()
+
+        # period_grid = np.linspace(min_period, max_period, len(df["period"].unique()))
+        # radius_grid = np.round(np.linspace(min_rad, max_rad, len(df["radius"].unique())), 2)
+        # bins = [period_grid, radius_grid]
+        # h1, x, y = np.histogram2d(df['period'][df['found'] == 1], df['radius'][df['found'] == 1], bins=bins)
+        # h2, x, y = np.histogram2d(df['period'][df['found'] == 0], df['radius'][df['found'] == 0], bins=bins)
+        # normed_hist = (100. * h1 / (h1 + h2))
+        # fig, ax = plt.subplots(figsize=(2.7 * 5, 5))
+        # im = plt.imshow(normed_hist.T, origin='lower', extent=(x[0], x[-1], y[0], y[-1]), interpolation='none',
+        #                 aspect='auto', cmap='viridis', vmin=0, vmax=100, rasterized=True)
+        # plt.colorbar(im, label='Recovery rate (%)')
+        # plt.xlabel('Injected period (days)')
+        # plt.ylabel(r'Injected radius (R$_\oplus$)')
+        # ax.set_title(object_id + " - P/R recovery (" + str(phases) + " " + phases_str + ")")
+        # if xticks is not None:
+        #     plt.xticks(xticks)
+        # else:
+        #     period_ticks_decimals = MATRIX.num_of_zeros(max_period - min_period) + 1
+        #     plot_bins = 10 if 10 < len(period_grid) else len(period_grid)
+        #     plt.locator_params(axis="x", nbins=plot_bins)
+        #     ax.xaxis.set_major_formatter(FormatStrFormatter('%.' + str(period_ticks_decimals) + 'f'))
+        # if yticks is not None:
+        #     plt.xticks(yticks)
+        # plt.savefig(inject_dir + '/inj-rec.png', bbox_inches='tight', dpi=200)
+        # plt.close()
 
     def __search(self, time, flux, rstar, rstar_min, rstar_max, mass, mstar_min, mstar_max, ab, intransit, epoch,
                  period, min_period, max_period, min_snr, cores, transit_template, ws, transits_min_count,
@@ -500,6 +527,8 @@ class MATRIX:
 
     @staticmethod
     def num_of_zeros(n):
+        if n.is_integer():
+            return 0
         s = '{:.16f}'.format(n).split('.')[1]
         return len(s) - len(s.lstrip('0'))
 
