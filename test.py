25fafb954c0f804372a849ffaccc32e67be79bc7
martin.dvpj <martin.devora.pajares@gmail.com>
2021-11-09 17:27:27 +0000

    fix tests

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 from tkmatrix.tkmatrix_class import MATRIX
 
 
-MATRIX.plot_results("TIC 273369281", "tkmatrix/tests/TIC-1234", 1)
+#MATRIX.plot_results("TIC 169285097", "/home/martin/Desktop/run_tests/tests/TIC169285097_ir_none", 1)
+MATRIX.plot_diff("TIC 169285097", "/home/martin/Desktop/run_tests/tests/TIC169285097_ir_sasg",
+                 "/home/martin/Desktop/run_tests/tests/TIC169285097_ir_none",
+                 "./")
