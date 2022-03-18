e32ff566d4a79c3d67907cdc5284671037f3936d
martin.dvpj <martin.devora.pajares@gmail.com>
2022-03-18 10:35:39 +0000

    update test

--- tkmatrix/tests/test_tkmatrix.py
+++ tkmatrix/tests/test_tkmatrix.py
@@ -1,10 +1,8 @@
-import multiprocessing
 import os
 import shutil
 import unittest
 import pytest
 from lcbuilder.star.starinfo import StarInfo
-from foldedleastsquares import transitleastsquares
 
 from tkmatrix.tkmatrix_class import MATRIX
 
@@ -16,7 +14,7 @@ class TestsMatrix(unittest.TestCase):
         inject_dir = None
         try:
             inject_dir = matrix.inject(1, 5, 5, 1, 3, 3, 1)
-            self.assertEquals(7, len(os.listdir(inject_dir)))
+            self.assertEquals(8, len(os.listdir(inject_dir)))
             self.assertEquals([1], matrix.sectors)
             self.assertAlmostEqual(0.47, matrix.mass, 2)
             self.assertAlmostEqual(0.44, matrix.massmin, 2)
@@ -33,9 +31,9 @@ class TestsMatrix(unittest.TestCase):
             self.assertAlmostEqual(0.076, matrix.rstar_min.value, 3)
             self.assertAlmostEqual(0.284, matrix.rstar_max.value, 3)
             self.assertEquals(".", matrix.dir)
-            matrix.recovery(inject_dir, 5, 0, oversampling=0.1)
+            matrix.recovery(inject_dir, 5, detrend_ws=0, oversampling=0.1)
             matrix.plot_results(target, inject_dir)
-            self.assertEquals(11, len(os.listdir(inject_dir)))
+            self.assertEquals(12, len(os.listdir(inject_dir)))
         finally:
             if inject_dir is not None:
                 shutil.rmtree(inject_dir, ignore_errors=True)
@@ -46,9 +44,9 @@ class TestsMatrix(unittest.TestCase):
         inject_dir = None
         try:
             inject_dir = matrix.inject(1, 5, 5, 1, 3, 3, 1)
-            matrix.recovery(inject_dir, 5, 0, oversampling=0.1)
+            matrix.recovery(inject_dir, 5, detrend_ws=0, oversampling=0.1)
             matrix.plot_results(target, inject_dir)
-            self.assertEquals(12, len(os.listdir(inject_dir)))
+            self.assertEquals(13, len(os.listdir(inject_dir)))
         finally:
             if inject_dir is not None:
                 shutil.rmtree(inject_dir, ignore_errors=True)
@@ -59,7 +57,7 @@ class TestsMatrix(unittest.TestCase):
         inject_dir = None
         try:
             inject_dir = matrix.inject(1, 5, 5.1, 2, 3, 3.1, 2)
-            self.assertEquals(10, len(os.listdir(inject_dir)))
+            self.assertEquals(11, len(os.listdir(inject_dir)))
         finally:
             if inject_dir is not None:
                 shutil.rmtree(inject_dir, ignore_errors=True)
@@ -70,27 +68,10 @@ class TestsMatrix(unittest.TestCase):
         inject_dir = None
         try:
             inject_dir = matrix.inject(2, 5, 5, 1, 3, 3, 1)
-            self.assertEquals(8, len(os.listdir(inject_dir)))
+            self.assertEquals(9, len(os.listdir(inject_dir)))
         finally:
             if inject_dir is not None:
                 shutil.rmtree(inject_dir, ignore_errors=True)
-
-    def test_transit_mask(self):
-        target = "TIC 305048087"
-        matrix = MATRIX(target, [2], ".", exposure_time=120)
-        inject_dir = matrix.retrieve_object_data()
-        power_args = {"transit_template": "default", "period_min": 1, "period_max": 10,
-                      "n_transits_min": 2, "show_progress_bar": True, "use_threads": multiprocessing.cpu_count() - 1,
-                      "R_star": matrix.radius, "R_star_min": matrix.radiusmin, "R_star_max": matrix.radiusmax,
-                      "M_star": matrix.mass, "M_star_min": matrix.massmin, "M_star_max": matrix.massmax}
-        intransit = matrix.transit_masks([{"P": 5.43, "T0": 2458355.249756 - 2457000.0, "D": 1.172585}],
-                                        matrix.lc_build.lc.time.value)
-        model = transitleastsquares(matrix.lc_build.lc.time.value, matrix.lc_build.lc.flux.value)
-        results = model.power(**power_args)
-        self.assertAlmostEqual(5.43, results.period, 2)
-        model = transitleastsquares(matrix.lc_build.lc.time.value[~intransit], matrix.lc_build.lc.flux.value[~intransit])
-        results = model.power(**power_args)
-        self.assertNotAlmostEqual(5.43, results.period, 2)
 
     def test_inject_inputs(self):
         target = "TIC 305048087"
@@ -152,7 +133,7 @@ class TestsMatrix(unittest.TestCase):
         inject_dir = None
         try:
             inject_dir = matrix.inject(1, 5, 5, 1, 3, 3, 1)
-            self.assertEquals(7, len(os.listdir(inject_dir)))
+            self.assertEquals(8, len(os.listdir(inject_dir)))
             self.assertEquals((0.2, 0.5), matrix.star_info.ld_coefficients)
             self.assertEquals(2000, matrix.star_info.teff)
             self.assertAlmostEqual(0.7, matrix.star_info.mass)
@@ -168,7 +149,7 @@ class TestsMatrix(unittest.TestCase):
         inject_dir = None
         try:
             inject_dir = matrix.inject(1, 5, 5, 1, 3, 3, 1)
-            self.assertEquals(7, len(os.listdir(inject_dir)))
+            self.assertEquals(8, len(os.listdir(inject_dir)))
             self.assertEquals((0.1258, 0.235), matrix.star_info.ld_coefficients)
             self.assertEquals(31000.0, matrix.star_info.teff)
             self.assertAlmostEqual(0.47, matrix.star_info.mass)
