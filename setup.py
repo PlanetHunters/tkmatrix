e72de092c0b04bd967ecc5461b9ef85175ed0810
martin.dvpj <martin.devora.pajares@gmail.com>
2022-05-08 22:03:39 +0100

    Preparing release 0.3.17

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-version = "0.3.16"
+version = "0.3.17"
 setuptools.setup(
     name="tkmatrix",
     version=version,
