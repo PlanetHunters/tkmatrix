36f70fc503151035c196b2e92fe0aebadba387ad
martin.dvpj <martin.devora.pajares@gmail.com>
2022-10-05 11:42:52 +0100

    add rv injection recovery

---
+++ tkmatrix/inject_rv_model.py
@@ -0,0 +1,43 @@
+import logging
+import os
+
+from ellc import lc
+import numpy as np
+import pandas as pd
+import astropy.constants as ac
+import astropy.units as u
+
+from tkmatrix.rv import RvFitter
+
+
+class InjectRvModel:
+    def __init__(self, inject_dir, time, rv, rv_err, rstar, mstar, t0, period, mplanet):
+        self.inject_dir = inject_dir
+        self.time = time
+        self.flux = rv
+        self.flux_err = rv_err
+        self.rstar = rstar
+        self.mstar = mstar
+        self.t0 = t0
+        self.period = period
+        self.mplanet = mplanet
+
+    @staticmethod
+    def make_model(inject_model):
+        """
+        Creates an injected RV curve given the provided InjectRvModel parameters instance.
+        :param inject_model: the InjectRvModel object to be used as RV source. The result
+        is written into a csv file.
+        """
+        logging.info('RV P = ' + str(inject_model.period) + ' days, Rp = ' + str(inject_model.rplanet) + ", T0 = " +
+                     str(inject_model.t0))
+        injected_rv = RvFitter.inject_rv(inject_model.time, inject_model.mstar, inject_model.rstar,
+                                         inject_model.mplanet, inject_model.planet_mass, inject_model.period,
+                                         inject_model.t0)
+        file_name = os.path.join(inject_model.inject_dir + '/RV_P' + str(inject_model.period) + '_R' +
+                                 str(inject_model.rplanet.value) + '_' + str(inject_model.t0) + '.csv')
+        lc_df = pd.DataFrame(columns=['bjd', 'rv', 'rv_err'])
+        lc_df['bjd'] = inject_model.time
+        lc_df['rv'] = inject_model.rv + injected_rv
+        lc_df['rv_err'] = inject_model.rv_err
+        lc_df.to_csv(file_name, index=False)
--- tkmatrix/rv.py
+++ tkmatrix/rv.py
@@ -1,195 +1,243 @@
 import os
+from multiprocessing import Pool
 
-import math
-import glob
-import numpy as np
-import pylab as pl
-import pandas as pd
-from iminuit import Minuit
-import iminuit
-#from astropy import units as u
-#from astropy import constants as const
-#PJA uncomments these two previous imports
 from astropy import units as u
-from astropy import constants as const
-import time
-
-#from scipy import stats
-#from scipy import optimize
-#from scipy.optimize import minimize
-
+import astropy.constants as ac
 import matplotlib.pyplot as plt
-from matplotlib.colors import LinearSegmentedColormap
-import matplotlib.colors as colors
-
-
-# ## Classes
-class nf(float):
-    def __repr__(self):
-        s = f'{self:.1f}'
-        return f'{self:.0f}' if s[-1] == '0' else s
+import numpy
+import ellc
+import pandas
+import scipy
+from scipy.optimize import differential_evolution
 
 
 class RvFitter:
-
     def __init__(self) -> None:
         super().__init__()
 
     @staticmethod
-    def axisToPeriod(a, M_star):
-        """
-        Converts from semimajor axis to period in days
-        :param M_star: the star mass
-        :return: the period in days
-        """
-        return np.sqrt(4. * (np.pi**2.) * (a ** 3.) / ((const.G.to(u.AU ** 3 * u.solMass ** (-1) * u.d ** (-2))).value * M_star))
+    def inject_rv(times, star_mass, rstar, planet_mass, rplanet, period, t0):
+        star_mass = star_mass * u.M_sun
+        rstar = rstar * u.R_sun
+        rplanet = rplanet * u.R_sun
+        planet_mass = planet_mass * u.M_sun
+        period_days = period * u.day
+        a = numpy.cbrt((ac.G * star_mass * period_days ** 2) / (4 * numpy.pi ** 2)).to(u.au)
+        model = ellc.rv(
+            t_obs=times,
+            radius_1=rstar.to(u.au) / a,  # star radius convert from AU to in units of a
+            radius_2=rplanet.to(u.au) / a,  # convert from Rearth (equatorial) into AU and then into units of a
+            sbratio=0,
+            incl=90,
+            t_zero=t0,
+            period=period_days.value,
+            a=a.to(u.R_sun).value,
+            q=planet_mass / star_mass,
+            f_c=None, f_s=None,
+            gdc_1=None, gdc_2=None,
+            didt=None,
+            domdt=None,
+            rotfac_1=1, rotfac_2=1,
+            hf_1=1.5, hf_2=1.5,
+            bfac_1=None, bfac_2=None,
+            heat_1=None, heat_2=None,
+            lambda_1=None, lambda_2=None,
+            vsini_1=None, vsini_2=None, n_int=None,
+            grid_1='default', grid_2='default',
+            ld_1='quad', ld_2=None,
+            shape_1='sphere', shape_2='sphere',
+            spots_1=None, spots_2=None, flux_weighted=False, verbose=1)
+        injected_rv = numpy.full(len(times), 0) + model[1]
+        return injected_rv
 
     @staticmethod
-    def grid_logSpaced(num_measures, order_ini, order_fin_in):
-        """
-        Creates a log-10 period grid
-        :param num_measures: number of measurements (density of the grid)
-        :param order_ini: the initial power of the logarithm
-        :param order_fin_in: the final power of the logarithm
-        :return: the period grid
-        """
-        grid_list = []
-        for order_fin in np.arange(order_ini + 1, order_fin_in, 1):
-            if order_fin < order_fin_in - 1:
-                grid_list.extend(np.logspace(order_ini, order_fin, num_measures, endpoint=False))
-            else:
-                grid_list.extend(np.logspace(order_ini, order_fin, num_measures, endpoint=True))
-            order_ini = order_fin
-        return grid_list
+    def sinfunc(t, k, omega, period):
+        return k * numpy.cos(omega + t * 2 * numpy.pi / period)
 
     @staticmethod
-    def plot_rv(file_output, rv, rv_error, bjd):
-        """
-        Plots the RV data against the BJD
-        :param file_output: Output file
-        :param rv: the RV values
-        :param rv_error: the RV error values
-        :param bjd: the BJD data
-        """
-        fig = plt.figure(figsize=(10, 6), dpi=200) # Create a figure of size 8x6 inches, 200 dots per inch
-        ax = fig.add_subplot(1, 1, 1)
-        ax.errorbar(bjd, rv, yerr=rv_error, fmt="o")
-        ax.set_title("RVs with RMS (m/s) = {}".format(round(np.sqrt(np.mean(rv ** 2)), 2)))
-        ax.set_xlabel('BJD (days)', fontsize=15)
-        ax.set_ylabel('RV (m/s)', fontsize=15)
-        plt.savefig(file_output)
-        plt.show()
+    def compute_semiamplitude_from_mmin(mmin, period, star_mass):
+        return mmin / ((star_mass ** (2. / 3.)) * (period ** (1. / 3.)) / 0.64)
 
     @staticmethod
-    def plot_fit(tau, m_min, fit_file):
-        """
-        Plot the fits results
-        :param tau: period grid
-        :param m_min: the Mass_min values
-        :param fit_file: the file of the output
-        """
-        fig = plt.figure(figsize=(10, 6), dpi=200)
-        ax = fig.add_subplot(1, 1, 1)
-        ax.plot(tau, m_min, "-", color='Black', linewidth=0.75, label="Thin fit, M$_m$(P)")
-        plt.plot([0, 50], [3, 3], color="red", linewidth=1.0, linestyle="-.")
-        plt.plot([0, 50], [10, 10], color="red", linewidth=1.0, linestyle="-.")
-        plt.plot([0, 50], [30, 30], color="red", linewidth=1.0, linestyle="-.")
-        plt.plot([0, 50], [100, 100], color="red", linewidth=1.0, linestyle="-.")
-        plt.plot([0, 50], [300, 300], color="red", linewidth=1.0, linestyle="-.")
-        plt.plot([0, 50], [1000, 1000], color="red", linewidth=1.0, linestyle="-.")
-        plt.plot([50, 50], [3, 1000], color="red", linewidth=1.0, linestyle="-.")
-        plt.text(60, 300, '50 days', fontsize=10, rotation=90, rotation_mode='anchor')
-        ax.set_title("M_min fits")
-        ax.set_xlabel('$P_{planeta} (days)$', fontsize=15)
-        ax.set_ylabel('$Max(Msin i/M_{\oplus})$', fontsize=15)
-        ax.set_xscale('log')
-        ax.set_yscale('log')
-        ax.set_xlim(1, 10 ** 4)
-        ax.set_ylim(0.1, 13 ** 3)
-        ax.legend(loc='upper right', fontsize=10)
-        plt.savefig(fit_file)
-        plt.show()
-
-    @staticmethod
-    def compute_mmin_from_semiamplitude(k_fit, m_s, tau):
-        """
-        Computes the M_min values for each k_fit-tau pairs.
-        :param k_fit: the fitted semi-amplitude
-        :param m_s: the star mass
-        :param tau: the period grid
-        :return: the M_min values
-        """
+    def compute_mmin_from_semiamplitude(period_grid, k_grid, star_mass):
         m_min = []
-        for i in np.arange(0, len(tau), 1):
-            m_min_value = k_fit[i] * (m_s ** (2./3.)) * (tau[i] ** (1./3.)) / 0.64
+        for i in numpy.arange(0, len(period_grid), 1):
+            m_min_value = k_grid[i] * (star_mass ** (2./3.)) * (period_grid[i] ** (1./3.)) / 0.64
             m_min.append(m_min_value)
         return m_min
 
     @staticmethod
-    def least_squares_search_comp(period, k, omega, bjd, rv, rv_error):
-        """
-        Function used to fit the semi-amplitude k.
-        :param period: the period for the fit
-        :param k: the semi-amplitude
-        :param omega: the initial phase
-        :param bjd: the BJD time
-        :param rv: the RV data
-        :param rv_error: the RV error data
-        :return: the least-squares sum to be minimized
-        """
-        return sum((rv - k * np.cos(omega + bjd * 2 * np.pi / period)) ** 2 / rv_error ** 2)
+    def running_median(data, kernel):
+        """Returns sliding median of width 'kernel' and same length as data """
+        idx = numpy.arange(kernel) + numpy.arange(len(data) - kernel + 1)[:, None]
+        med = numpy.median(data[idx], axis=1)
+
+        # Append the first/last value at the beginning/end to match the length of
+        # data and returned median
+        first_values = med[0]
+        last_values = med[-1]
+        missing_values = len(data) - len(med)
+        values_front = int(missing_values * 0.5)
+        values_end = missing_values - values_front
+        med = numpy.append(numpy.full(values_front, first_values), med)
+        med = numpy.append(med, numpy.full(values_end, last_values))
+        return med
 
     @staticmethod
-    def k_fit_funct(path_output, massStarList, rv, rv_error, bjd, tau):
-        """
-        Function that calculates a fit for RV data using iMINUIT minimizator (https://iminuit.readthedocs.io). There are also
-        some other functions defined inside for plotting and other purpose. We decided to define them here because this allows
-        better executions times than define them outside
+    def spectra(chi2, oversampling_factor=1, kernel_size=30):
+        SR = numpy.min(chi2) / chi2
+        SDE_raw = (1 - numpy.mean(SR)) / numpy.std(SR)
 
-        Parameters:
-        -----------
-        path_output (str)
-        pathOutFit (str)
-        massStarList (list)
-        len_file (int)
-        rv(list of list)
-        rv_error(list of list)
-        bjd(listof list)
-        tau(list)
-        tau_coarse (list)
-        tau_coarse_plot(list)
-        names_plot (list)
+        # Scale SDE_power from 0 to SDE_raw
+        power_raw = SR - numpy.mean(SR)  # shift down to the mean being zero
+        scale = SDE_raw / numpy.max(power_raw)  # scale factor to touch max=SDE_raw
+        power_raw = power_raw * scale
 
-        Returns:
-        -----------
+        # Detrended SDE, named "power"
+        kernel = oversampling_factor * kernel_size
+        if kernel % 2 == 0:
+            kernel = kernel + 1
+        if len(power_raw) > 2 * kernel:
+            my_median = RvFitter.running_median(power_raw, kernel)
+            power = power_raw - my_median
+            # Re-normalize to range between median = 0 and peak = SDE
+            # shift down to the mean being zero
+            power = power - numpy.mean(power)
+            SDE = numpy.max(power / numpy.std(power))
+            # scale factor to touch max=SDE
+            scale = SDE / numpy.max(power)
+            power = power * scale
+        else:
+            power = power_raw
+            SDE = SDE_raw
 
-        k_fit_list, mMinList, mEnvelopeList, mEnvelopePlotList
-        and some plots in the indicated paths
-        """
-        k_fit = []
-        rv_meas = np.array(rv)
-        rv_meas_error = np.array(rv_error)
-        bjd = np.array(bjd)
-        plot_file = path_output + "/RV.png"
-        fit_file = path_output + "/M_min_fit.png"
-        RvFitter.plot_rv(plot_file, rv_meas, rv_meas_error, bjd)
-        #TODO parallelize
-        for index, orb_period in enumerate(tau):
-            min_period = tau[index]
-            max_period = tau[index]
-            m = Minuit(RvFitter.least_squares_search_comp,
-                       period=orb_period, k=1, omega=1, limit_period=(min_period, max_period), rv=rv_meas,
-                       rv_error=rv_meas_error, pedantic=False)
-            m.migrad()  # finds minimum of least_squares function
-            m.hesse()  # computes errors
-            if not m.get_fmin()[7]:  # if enters here it means that minimization do not converge
-                # CAREFULL if it does not enter does not mean the minimization is good...
-                print("Warning do not converge. Revise the program or the star")
-                print("Period = ", orb_period)
-                #                pprint(m.get_fmin())
+        return SR, power_raw, power, SDE_raw, SDE
+
+    @staticmethod
+    def recover_signal(rv_df, period, msin, omega, rv_masks, star_mass, period_grid_geom='lin', steps_period=None,
+                       period_min=0.5, max_period=None, min_snr=5, min_sde=5, max_runs=5, cpus=os.cpu_count() - 1):
+        df = rv_df.copy()
+        sde = numpy.inf
+        snr = numpy.inf
+        found = False
+        signal_period = 0
+        signal_k = 0
+        signal_omega = 0
+        signal_msin = 0
+        for run in numpy.arange(0, max_runs):
+            period_grid, k_grid, omega_grid, msin_grid, least_squares_grid, argmax, power, snr, SDE = \
+                RvFitter.recover_periods(df, period_grid_geom, steps_period, period_min, max_period, rv_masks,
+                                         star_mass, cpus)
+            signal_omega = omega_grid[argmax]
+            signal_k = k_grid[argmax]
+            signal_msin = msin_grid[argmax]
+            signal_period = period_grid[argmax]
+            period_diff = numpy.abs(period - signal_period)
+            msin_diff = numpy.abs(msin - signal_msin)
+            omega_diff = numpy.abs(omega - signal_omega)
+            omega_diff = omega_diff if omega_diff < numpy.pi else numpy.pi - numpy.abs(omega - signal_omega)
+            found = period_diff < period * 0.01 and msin_diff < msin * 0.1
+            if found or snr < min_snr or SDE < min_sde:
                 break
-            k_fit.append(np.abs(m.values["k"]))
-        m_min = RvFitter.compute_mmin_from_semiamplitude(k_fit, massStarList, tau)
-        #TODO store M_min together to K
-        RvFitter.plot_fit(tau, m_min, fit_file)
-        return k_fit, m_min
+            else:
+                rv_fit = RvFitter.sinfunc(df["bjd"], signal_k, signal_omega, signal_period)
+                df["rv"] = df["rv"] - rv_fit
+        return found, run, snr, sde, signal_period, signal_omega, signal_msin
+
+    @staticmethod
+    def recover_period(input):
+        k = 0
+        k_err = 0
+        omega = 0
+        omega_err = 0
+        least_squares = 0
+        try:
+            if input.msin is not None and input.star_mass is not None:
+                k = RvFitter.compute_semiamplitude_from_mmin(input.msin, input.period, input.star_mass)
+                k_max = k + 0.01
+            else:
+                k = 0.
+                k_max = numpy.inf
+            popt, pcov = scipy.optimize.curve_fit(RvFitter.sinfunc, input.time, input.rv_data, sigma=input.rv_err,
+                                                  bounds=([k, 0., input.period], [k_max, numpy.pi, input.period + 0.00001]))
+            perr = numpy.sqrt(numpy.diag(pcov))
+            k_err, omega_err, period_err = perr
+            k, omega, period = popt
+            least_squares = numpy.sum((input.rv_data - RvFitter.sinfunc(input.time, k, omega, period)) ** 2 /
+                                      (input.rv_err ** 2))
+        except RuntimeError as e:
+            print("Runtime error for period " + str(input.period))
+        return (k, omega, k_err, omega_err, least_squares)
+
+    @staticmethod
+    def recover_periods(rv_df, period_grid_geom='lin', steps_period=None, period_min=0.5, max_period=None,
+                        rv_masks=None, star_mass=None, cpus=os.cpu_count() - 1):
+        if rv_masks is None:
+            rv_masks = []
+        if steps_period is None:
+            steps_period = int(rv_df["bjd"].max() - rv_df["bjd"].min())
+        if max_period is None:
+            max_period = (rv_df["bjd"].max() - rv_df["bjd"].min()) * 2
+        time = rv_df["bjd"].to_numpy()
+        rv_data = rv_df["rv"].to_numpy()
+        rv_err = rv_df["rv_err"].to_numpy()
+        rv_data = RvFitter.mask_signals(rv_df, rv_masks, star_mass)
+        period_grid = numpy.linspace(period_min, max_period, steps_period) if period_grid_geom == "lin" \
+            else numpy.logspace(numpy.log10(period_min), numpy.log10(max_period), steps_period)
+        k_grid = []
+        k_err_grid = []
+        omega_grid = []
+        omega_err_grid = []
+        least_squares_grid = []
+        k = 0
+        k_err = 0
+        omega = 0
+        omega_err = 0
+        least_squares = 0
+        inputs = []
+        for period in period_grid:
+            inputs.append(RecoverPeriodInput(time, rv_data, rv_err, period))
+        with Pool(processes=cpus) as pool:
+            recovered_outputs = pool.map(RvFitter.recover_period, inputs)
+        for output in recovered_outputs:
+            k_grid.append(output[0])
+            omega_grid.append(output[1])
+            k_err_grid.append(output[2])
+            omega_err_grid.append(output[3])
+            least_squares_grid.append(output[4])
+        least_squares_grid = numpy.array(least_squares_grid)
+        least_squares_grid[least_squares_grid == 0] = numpy.max(least_squares_grid)
+        msin_grid = RvFitter.compute_mmin_from_semiamplitude(period_grid, k_grid, star_mass)
+        SR, power_raw, power, SDE_raw, SDE = RvFitter.spectra(numpy.array(least_squares_grid))
+        sde_power = power / numpy.std(power)
+        argmax_sde = numpy.nanargmax(sde_power)
+        max_msin = msin_grid[argmax_sde]
+        period_max_msin = period_grid[argmax_sde]
+        snr_window_size = (max_period - period_min) / 20
+        indexes_around = numpy.argwhere((period_grid > period_max_msin - snr_window_size / 2) &
+                                        (period_grid < period_max_msin + snr_window_size / 2)).flatten()
+        median_power_around = numpy.nanmedian(numpy.array(msin_grid)[indexes_around])
+        snr = msin_grid[argmax_sde] / median_power_around
+        return rv_data, period_grid, k_grid, omega_grid, msin_grid, least_squares_grid, argmax_sde, power, snr, SDE
+
+    @staticmethod
+    def mask_signals(rv_df, rv_masks, star_mass):
+        time = rv_df["bjd"].to_numpy()
+        rv_data = rv_df["rv"].to_numpy()
+        rv_err = rv_df["rv_err"].to_numpy()
+        for rv_mask in rv_masks:
+            k, omega, k_err, omega_err, least_squares = \
+                RvFitter.recover_period(RecoverPeriodInput(time, rv_data, rv_err, rv_mask['P'],
+                                                           rv_mask['M'] if 'M' in rv_mask else None, star_mass))
+            rv_subtract = RvFitter.sinfunc(time, k, omega, rv_mask['P'])
+            rv_data = rv_data - rv_subtract
+        return rv_data
+
+class RecoverPeriodInput:
+    def __init__(self, time, rv_data, rv_err, period, msin=None, star_mass=None):
+        self.time = time
+        self.rv_data = rv_data
+        self.rv_err = rv_err
+        self.period = period
+        self.msin = msin
+        self.star_mass = star_mass
