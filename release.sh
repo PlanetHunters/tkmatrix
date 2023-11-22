907708387b48d2049b825b2b810093fd2f6637b1
martin.dvpj <martin.devora.pajares@gmail.com>
2023-11-22 19:06:50 +0000

    upgrade python and lcbuilder

--- release.sh
+++ release.sh
@@ -22,13 +22,13 @@ if ! [[ -z ${tests_results} ]]; then
   rm -r tkmatrix-reqs
   rm -R *egg-info
   set -e
-  python3.9 -m venv tkmatrix-reqs
+  python3.10 -m venv tkmatrix-reqs
   source tkmatrix-reqs/bin/activate
-  python3.9 -m pip install pip -U
-  python3.9 -m pip install numpy==1.23.5
+  python3.10 -m pip install pip -U
+  python3.10 -m pip install numpy==1.23.5
   sed -i '5s/.*/version = "'${git_tag}'"/' setup.py
-  python3.9 -m pip install -e .
-  python3.9 -m pip list --format=freeze > requirements.txt
+  python3.10 -m pip install -e .
+  python3.10 -m pip list --format=freeze > requirements.txt
   deactivate
   git add requirements.txt
   git add setup.py
