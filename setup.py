bebad88bcdc3e853afa48f1f86f4a39435ee8644
martindevora <martin.devora.pajares@gmail.com>
2024-01-05 21:58:56 +0000

    Preparing release 0.8.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-version = "0.8.1"
+version = "0.8.2"
 setuptools.setup(
     name="tkmatrix",
     version=version,
